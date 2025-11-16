5b44ba6fae3413b3a50f2bd878e8d9016c275d19
Cesar Garcia <cesar.garcia.suarez@gmail.com>
2025-11-15 20:44:11 -0500

    Proyecto pagina web

---
+++ javascript/js/script.js
@@ -0,0 +1,31 @@
+//alert("Hola mundo");
+
+let nombre = "Maria";
+let numero = 100;
+let direccion = "Calle 16A #14-26";
+let estado = false;
+let notaFinal = 4.5;
+
+console.log("Nombre: " + nombre);
+console.log("Numero: " + numero);
+console.log("dirección: " + direccion);
+console.log("Estado: " + estado);
+console.log("Nota final: " + notaFinal);
+
+// Operadores
+let numero1 = 10;
+let numero2 = 5;
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplica = numero1 * numero2;
+let divide = numero1 / numero2;
+let modulo = numero1 % numero2;
+let potencia = numero1 ** numero2;
+
+console.log("La suma es: " + suma);
+console.log("La resta es: " + resta);
+console.log("La multiplicación es: " + multiplica);
+console.log("La división es: " + divide);
+console.log("El modulo es: " + modulo);
+console.log("La potencia es: " + potencia);
